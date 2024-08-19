24fe40282453301b9d5ce25933c055b619808ea9
JhonmaSG <jmserranog@udistrital.edu.co>
2024-08-19 18:49:44 -0500

    Fix v

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 // npm install node-fetch@2
 const fetch = require("node-fetch");
 const request = require('request');
+const axios = require('axios');
 
 require('dotenv').config()
 
@@ -26,31 +27,28 @@ app.get("/", (req, res) => {
 
 // POST route
 app.post("/convert-mp3", async (req, res) => {
-
   const videoId = req.body.videoId;
-  
-  if(
-    videoId === undefined ||
-    videoId === "" ||
-    videoId === null
-  ){
-    return res.render("index", { success : false, message : "Please enter a video ID"});
-  } else {
-    
-    const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, {
-      "method": "GET",
-      "headers": {
+
+  if (!videoId) {
+    return res.render("index", { success: false, message: "Please enter a video ID" });
+  }
+
+  try {
+    const response = await axios.get(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, {
+      headers: {
         "x-rapidapi-key": process.env.API_KEY,
         "x-rapidapi-host": process.env.API_HOST
-        }
+      }
     });
 
-    const fetchResponse = await fetchAPI.json();
- 
-    if(fetchResponse.status === "ok")
-      return res.render("index",{ success : true,  song_title : fetchResponse.title, song_link : fetchResponse.link})
-    else
-      return res.render("index", { success : false, message : fetchResponse.msg});
+    if (response.data.status === "ok") {
+      return res.render("index", { success: true, song_title: response.data.title, song_link: response.data.link });
+    } else {
+      return res.render("index", { success: false, message: response.data.msg });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.render("index", { success: false, message: "An error occurred while processing the request" });
   }
 });
 
